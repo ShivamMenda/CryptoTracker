b5397837ea4bdc67a7e0a8c8e5fc6f1a6601124b
Shivam Menda <74780977+ShivamMenda@users.noreply.github.com>
2021-05-26 20:05:34 +0530

    Adding All Files

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'price_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF000000),
+        scaffoldBackgroundColor: Color(0xFF121212),
+      ),
+      home: PriceScreen(),
+    );
+  }
+}
